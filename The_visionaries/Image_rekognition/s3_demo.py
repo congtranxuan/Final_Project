b4676f2ffe843df86d68e0e99535dee11198bab6
unknown <congtranxuan@gmail.com>
2019-11-16 01:03:06 -0600

    update

--- The_visionaries/Image_rekognition/s3_demo.py
+++ The_visionaries/Image_rekognition/s3_demo.py
@@ -15,9 +15,9 @@ def download_file(file_name, bucket):
     """
     Function to download a given file from an S3 bucket
     """
-    s3 = boto3.resource('s3')
+    s3_client = boto3.client('s3')
     output = f"downloads/{file_name}"
-    s3.Bucket(bucket).download_file(file_name, output)
+    s3_client.download_file(file_name, output)
 
     return output
 
